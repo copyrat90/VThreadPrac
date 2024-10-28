8b6cbab28aafbb5ca8b5b1c39215f95438b71aff
copyrat90 <copyrat90@gmail.com>
2024-10-28 16:55:05 +0900

    Deal with spurious wakeup

--- 04_pseudo_producer_consumer/main.cpp
+++ 04_pseudo_producer_consumer/main.cpp
@@ -3,9 +3,12 @@
 
 #include <array>
 #include <atomic>
+#include <chrono>
 #include <format>
 #include <iostream>
 #include <random>
+
+using Clock = std::chrono::steady_clock;
 
 int g_Data = 0;
 int g_Connect = 0;
@@ -13,11 +16,39 @@ bool g_Shutdown = false;
 
 SRWLOCK g_Connect_lock;
 
+/// @return shutdown 해야하는지 여부
+[[nodiscard]] bool sleep_shutdown_safe(int sleep_duration)
+{
+    bool shutdown_false = false;
+    bool shutdown = false;
+
+    // Spurious wakeup(가짜 깨어남) 발생 시 대응하기 위한 반복
+    for (;;)
+    {
+        auto prev_time = Clock::now();
+        WaitOnAddress(&g_Shutdown, &shutdown_false, sizeof(g_Shutdown), sleep_duration);
+        auto slept_duration = Clock::now() - prev_time;
+
+        // g_Shutdown 에 새로운 값이 쓰였다면, 반영
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+        shutdown = g_Shutdown;
+
+        // 제대로 깨어났으면, 반복문 탈출
+        if (shutdown || slept_duration >= std::chrono::milliseconds(sleep_duration))
+            break;
+        // 가짜로 깨어났다면, 남은 시간 더 자러 감
+        else
+            sleep_duration -=
+                static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(slept_duration).count());
+    }
+
+    return shutdown;
+}
+
 unsigned __stdcall acceptor(void*)
 {
     std::mt19937 rng(std::random_device{}());
     std::uniform_int_distribution<int> dist(100, 1000);
-    bool shutdown_false = false;
 
     for (;;)
     {
@@ -25,12 +56,9 @@ unsigned __stdcall acceptor(void*)
         ++g_Connect;
         ReleaseSRWLockExclusive(&g_Connect_lock);
 
-        const auto sleep_time = dist(rng);
-        WaitOnAddress(&g_Shutdown, &shutdown_false, sizeof(g_Shutdown), sleep_time);
-
-        // g_Shutdown 에 새로운 값이 쓰였다면, 반영
-        std::atomic_thread_fence(std::memory_order_seq_cst);
-        if (g_Shutdown)
+        const auto sleep_duration = dist(rng);
+        const bool shutdown = sleep_shutdown_safe(sleep_duration);
+        if (shutdown)
             break;
     }
 
@@ -42,7 +70,6 @@ unsigned __stdcall disconnector(void*)
 {
     std::mt19937 rng(std::random_device{}());
     std::uniform_int_distribution<int> dist(100, 1000);
-    bool shutdown_false = false;
 
     for (;;)
     {
@@ -51,12 +78,9 @@ unsigned __stdcall disconnector(void*)
             --g_Connect;
         ReleaseSRWLockExclusive(&g_Connect_lock);
 
-        const auto sleep_time = dist(rng);
-        WaitOnAddress(&g_Shutdown, &shutdown_false, sizeof(g_Shutdown), sleep_time);
-
-        // g_Shutdown 에 새로운 값이 쓰였다면, 반영
-        std::atomic_thread_fence(std::memory_order_seq_cst);
-        if (g_Shutdown)
+        const auto sleep_duration = dist(rng);
+        const bool shutdown = sleep_shutdown_safe(sleep_duration);
+        if (shutdown)
             break;
     }
 
@@ -66,7 +90,7 @@ unsigned __stdcall disconnector(void*)
 
 unsigned __stdcall updater(void*)
 {
-    constexpr int SLEEP_TIME = 10;
+    constexpr int SLEEP_DURATION = 10;
     bool shutdown_false = false;
 
     for (;;)
@@ -75,11 +99,8 @@ unsigned __stdcall updater(void*)
         if (data % 1000 == 0)
             std::cout << std::format("g_Data: {}\n", data);
 
-        WaitOnAddress(&g_Shutdown, &shutdown_false, sizeof(g_Shutdown), SLEEP_TIME);
-
-        // g_Shutdown 에 새로운 값이 쓰였다면, 반영
-        std::atomic_thread_fence(std::memory_order_seq_cst);
-        if (g_Shutdown)
+        const bool shutdown = sleep_shutdown_safe(SLEEP_DURATION);
+        if (shutdown)
             break;
     }
 
